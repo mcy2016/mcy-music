00d3ac125b55a459294a8e2cdad6da81991268f5
mcy2016 <1991993249@qq.com>
2017-10-23 03:58:51 +0800

    完成header组件，router文件及各业务组件文件的新建

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,23 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <m-header></m-header>
   </div>
 </template>
 
-<script>
+<script type="text/ecmascript-6">
+import MHeader from 'components/m-header/m-header'
+
 export default {
-  name: 'app'
+  components: {
+    MHeader
+  }
 }
 </script>
 
-<style>
+<style scoped lang="stylus" rel="stylesheet/stylus">
+@import '~common/stylus/variable'
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: $color-theme;
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Recommend from 'components/recommend/recommend'
+import Rank from 'components/rank/rank'
+import Search from 'components/search/search'
+import Singer from 'components/singer/singer'
+import Tab from 'components/tab/tab'
 
 Vue.use(Router)
 
@@ -8,8 +12,28 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: HelloWorld
+      redirect: 'recommend'
+    },
+    {
+      path: '/recommend',
+      component: Recommend
+    },
+    {
+      path: '/singer',
+      component: Singer
+    },
+    {
+      path: '/rank',
+      component: Rank
+    },
+    {
+      path: '/search',
+      component: Search
+    },
+    {
+      path: '/tab',
+      component: Tab
     }
+
   ]
 })
